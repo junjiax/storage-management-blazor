17a69e2519fa3dbc2b9247ac4e10669e4a6dff50
HoangViet158 <kirito15082004@gmail.com>
2025-12-02 15:08:36 +0700

    xong models va service customer, supplier, promotion

--- Services/PromotionApi.cs
+++ Services/PromotionApi.cs
@@ -1,8 +1,25 @@
-﻿using System;
+﻿using frontendblazor.Models;
 
-public class Class1
+namespace frontendblazor.Services;
+
+public sealed class PromotionApi
 {
-	public Class1()
-	{
-	}
+    private readonly ApiClient apiClient;
+
+    public PromotionApi(ApiClient apiClient)
+    {
+        this.apiClient = apiClient;
+    }
+
+    public Task<ApiResponse<PromotionResponse>?> AddAsync(CreatePromotionRequest request, CancellationToken ct = default)
+        => apiClient.PostAsync<CreatePromotionRequest, ApiResponse<PromotionResponse>>("promotion", request, ct);
+
+    public Task<ApiResponse<PromotionResponse>?> UpdateAsync(UpdatePromotionRequest request,int id ,CancellationToken ct = default)
+        => apiClient.PutAsync<UpdatePromotionRequest, ApiResponse<PromotionResponse>>($"promotion/{id}", request, ct);
+	public Task<ApiResponse<PromotionResponse>?> GetAllAsync(CancellationToken ct = default)
+	=> apiClient.GetAsync<ApiResponse<PromotionResponse>>("promotion", ct);
+	public Task<ApiResponse<PromotionResponse>?> GetByIdAsync(int id, CancellationToken ct = default)
+	=> apiClient.GetByIdAsync<ApiResponse<PromotionResponse>>("promotion", id, ct);
+	public Task<ApiResponse<bool>?> DeleteAsync(int id, CancellationToken ct = default)
+    => apiClient.DeleteAsync<ApiResponse<bool>>("promotion", id, ct);
 }
