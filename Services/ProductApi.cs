a25ac468264d12c62cd402f019011842823794c0
vanmuoi24 <domuoigghh@gmail.com>
2025-12-09 14:52:03 +0700

    muoi

--- Services/ProductApi.cs
+++ Services/ProductApi.cs
@@ -1,5 +1,4 @@
-﻿
-using frontendblazor.Models;
+﻿using frontendblazor.Models;
 
 namespace frontendblazor.Services;
 
@@ -11,10 +10,10 @@ public sealed class ProductApi
     {
         this.apiClient = apiClient;
     }
+
     public Task<ApiResponse<List<ProductResponse>>?> GetAllAsync(CancellationToken ct = default)
         => apiClient.GetAsync<ApiResponse<List<ProductResponse>>>("product", ct);
 
-
+    public Task<ApiResponse<ProductResponse>?> GetByIdAsync(int id, CancellationToken ct = default)
+        => apiClient.GetAsync<ApiResponse<ProductResponse>>($"product/{id}", ct);
 }
-
-
